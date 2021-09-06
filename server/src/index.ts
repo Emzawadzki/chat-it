5d2a12521507b2e2a42ac2a4d999d211062a0907
Emzawadzki <emzawadzki@gmail.com>
2021-09-06 22:28:12 +0200

    feat: add database connection & simple user entity

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,27 @@
 import "reflect-metadata";
+import dotenv from "dotenv";
 
-console.log("I'm in")
+import { createConnection } from "typeorm";
+
+import { User } from "./entity/User";
+
+// @TODO: handle different environments
+dotenv.config({ path: "../development.env" });
+
+const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } = process.env;
+
+createConnection({
+  type: "mysql",
+  host: DB_HOST,
+  port: Number(DB_PORT),
+  username: DB_USER,
+  password: DB_PASS,
+  database: DB_NAME,
+  entities: [User],
+  synchronize: true,
+  logging: false,
+})
+  .then((connection) => {
+    // ...
+  })
+  .catch((error) => console.log(error));
