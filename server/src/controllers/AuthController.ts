e26c2f851320c14b51bae739d8d4541d7c26ca40
Emzawadzki <emzawadzki@gmail.com>
2021-09-08 20:41:29 +0200

    feat: add error handling to login controller

--- server/src/controllers/AuthController.ts
+++ server/src/controllers/AuthController.ts
@@ -1,26 +1,32 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { getRepository } from "typeorm";
 
 import { User } from "../entity/User";
 
 export class AuthController {
-  static login = async (request: Request, response: Response) => {
-    if (!request.body) {
-      return response.status(400).send();
-    }
-    const { username } = request.body;
-    if (!username) {
-      return response.status(400).send();
-    }
+  static login = async (
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) => {
+    try {
+      if (!request.body) {
+        return response.status(400).send();
+      }
+      const { username } = request.body;
+      if (!username) {
+        return response.status(400).send();
+      }
 
-    const userRepository = getRepository(User);
+      const userRepository = getRepository(User);
 
-    const userFound = await userRepository.findOne({ where: { username } });
-    if (!userFound) {
-      console.log("user not found");
-      return response.status(401).send();
+      const userFound = await userRepository.findOne({ where: { username } });
+      if (!userFound) {
+        return response.status(401).send();
+      }
+      return response.status(200).send();
+    } catch (e) {
+      next(e);
     }
-    console.log("user found!");
-    return response.status(200).send();
   };
 }
