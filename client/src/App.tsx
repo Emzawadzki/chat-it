79128f37e2746f5b142c8fb3e146490b01944c5a
Emzawadzki <emzawadzki@gmail.com>
2021-09-12 15:49:40 +0200

    feat: add base header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,12 @@
+import { Header } from './components/Header';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Header
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
+
 
 export default App;
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,7 @@
+import "./Header.css";
+
+export const Header: React.FC = () => {
+    return <header className="Header">
+        <h1 style={{ textAlign: "center" }}>Header</h1>
+    </header>
+}
