4caf1c0c770a3aa15189627080478fe562360688
Emzawadzki <emzawadzki@gmail.com>
2021-09-20 21:57:32 +0200

    feat(client): update API calls URL

--- client/src/api/BaseApi.ts
+++ client/src/api/BaseApi.ts
@@ -4,8 +4,8 @@ export class BaseApi {
   static baseUrl = "/api";
 
   static postRequest = async (url: string, body: object) =>
-    axios.post(url, body);
+    axios.post(this.baseUrl + url, body);
 
   static getRequest = async <ResponseType>(url: string) =>
-    axios.get<ResponseType>(url).then((res) => res.data);
+    axios.get<ResponseType>(this.baseUrl + url).then((res) => res.data);
 }
