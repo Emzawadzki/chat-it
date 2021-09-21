8e42a8cfe7ea40d0a861196c89ed0a0e6589ac23
Emzawadzki <emzawadzki@gmail.com>
2021-09-21 20:49:00 +0200

    feat(client): unset user on logout

--- client/src/components/Header/LogoutButton/LogoutButton.tsx
+++ client/src/components/Header/LogoutButton/LogoutButton.tsx
@@ -2,15 +2,18 @@ import { useMutation, useQueryClient } from "react-query";
 
 import { AuthApi } from "../../../api/AuthApi";
 import { QUERY } from "../../../config/queries";
+import { useUserContext } from "../../../providers/UserProvider";
 
 export const LogoutButton: React.FC = () => {
   const { mutate } = useMutation(AuthApi.logout);
   const { invalidateQueries } = useQueryClient();
+  const { setUser } = useUserContext();
 
   const handleClick = () => {
     mutate(undefined, {
       onSettled: () => {
-        invalidateQueries(QUERY.USER)
+        invalidateQueries(QUERY.USER);
+        setUser(null);
       }
     });
   }
