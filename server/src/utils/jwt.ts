b6894c418ae374091f021a14ae008ed70aa56f2c
Emzawadzki <emzawadzki@gmail.com>
2021-09-21 20:47:35 +0200

    feat(server): add token expiration time

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -1,7 +1,7 @@
 import { JwtPayload, sign, verify } from "jsonwebtoken";
 
 export const encodeJWT = (payload: string | object) => {
-  return sign(payload, process.env.JWT_ACCESS_TOKEN!);
+  return sign(payload, process.env.JWT_ACCESS_TOKEN!, { expiresIn: "7d" });
 };
 
 export const decodeJWT = <T extends { [key: string]: any }>(token: string) => {
