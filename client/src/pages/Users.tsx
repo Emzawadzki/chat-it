3614d3b65b3dd1892d7d911be920e3e019549578
Emzawadzki <emzawadzki@gmail.com>
2021-10-06 20:32:37 +0200

    feat(client): add link to chat with user

--- client/src/pages/Users.tsx
+++ client/src/pages/Users.tsx
@@ -1,14 +1,17 @@
 import { useQuery } from "react-query"
+import { Link } from "react-router-dom"
 
 import { UserApi } from "../api/UserApi"
 import { QUERY } from "../config/queries"
+import { useUserContext } from "../providers/UserProvider"
 
 export const Users: React.FC = () => {
-  const { data } = useQuery(QUERY.ALL_USERS, UserApi.getAll)
+  const { data } = useQuery(QUERY.ALL_USERS, UserApi.getAll);
+  const { user: currentUser } = useUserContext();
 
   return <ul>
-    {data?.users.map(user => (
-      <li key={user.id}>{user.name}</li>
+    {data?.users.filter(user => user.id !== currentUser!.id).map(user => (
+      <li key={user.id}><Link to={`/chat/${user.id}`}>{user.name}</Link></li>
     ))}
   </ul>
 }
